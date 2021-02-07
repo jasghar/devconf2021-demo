c218f47c22780568ecd4d5dfc6a8507aa71f9b88
Jabran Asghar <jasghar@adobe.com>
2021-02-07 13:22:55 +0100

    Updates

---
+++ precache-manifest.7707ee89e44be6eecc1903826b2da147.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf191fb0e055d0df9968b66274d2505e",
+    "url": "/devconf2021-demo/index.html"
+  },
+  {
+    "revision": "5e1929da00bf7419cdc5",
+    "url": "/devconf2021-demo/static/css/main.bd3d21f4.chunk.css"
+  },
+  {
+    "revision": "1b5f20d74ac050b3d80c",
+    "url": "/devconf2021-demo/static/js/2.5f896c34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/devconf2021-demo/static/js/2.5f896c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e1929da00bf7419cdc5",
+    "url": "/devconf2021-demo/static/js/main.e482bea0.chunk.js"
+  },
+  {
+    "revision": "9c47b250bf58d6173510",
+    "url": "/devconf2021-demo/static/js/runtime-main.2168cd2f.js"
+  },
+  {
+    "revision": "f227c489ca8dcfded430ecc5a71d600f",
+    "url": "/devconf2021-demo/static/media/icon-close.f227c489.svg"
+  },
+  {
+    "revision": "200bc7ed9cfe0f3bde2835852b50271e",
+    "url": "/devconf2021-demo/static/media/icon-loading.200bc7ed.svg"
+  },
+  {
+    "revision": "365933201d0bf2d24cee184d1f7083ce",
+    "url": "/devconf2021-demo/static/media/wknd-logo-dk.36593320.svg"
+  }
+]);
